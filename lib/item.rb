4d73e653213ddb05f6378d5c0195d47285ee983d
Adam Jonas <adamjonas25@gmail.com>
2021-01-21 09:55:57 +0000

    pass name getter method spec

--- lib/item.rb
+++ lib/item.rb
@@ -1,6 +1,8 @@
 module SimpleCheckout
   class Item
+    attr_reader :name
     def initialize(args = {})
+      @name = args.fetch(:name)
     end
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,12 +1,20 @@
 module SimpleCheckout
   describe Item do
-    context '#intialize'
-    it 'takes an empty hash as an argument' do
-      expect { Item.new{} }.to_not raise_error
+    context '#intialize' do
+      it 'raises an exception if an empty hash is provided as an argument' do
+        expect { Item.new{} }.to raise_error
+      end
+
+      it 'takes an argument of a hash with name and price' do
+        expect { Item.new(name: 'Milk', price: 50) }.to_not raise_error
+      end
     end
 
-    it 'takes an argument of a hash with name and price' do
-      expect { Item.new(name: 'Milk', price: 50) }.to_not raise_error
+    context '#name' do
+      before (:all) { @item = Item.new(name: 'Eggs', price: 200 ) }
+      it 'returns the items name' do
+        expect(@item.name).to eq 'Eggs'
+      end
     end
   end
 end
