6c56743f6239d699beb8f8ca19f65dad724ab216
Adam Jonas <adamjonas25@gmail.com>
2021-01-21 12:10:41 +0000

    refactor #scan and #remove_scanned_item to use #find_item_index

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -6,12 +6,13 @@ module SimpleCheckout
     end
 
     def scan(item_name)
-      item_index = basket.find_index { |item| item.name == item_name }
+      item_index = find_item_index(item_name)
       price = basket[item_index].price
     end
 
     def remove_scanned_item(item_name)
-      basket.delete_at(basket.find_index { |item| item.name == item_name })
+      item_index = find_item_index(item_name)
+      basket.delete_at(item_index)
     end
 
     def find_item_index(item_name)
