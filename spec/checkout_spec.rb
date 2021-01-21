09e27a72c72dd542b1aab169b9e5e4b54a449e04
Adam Jonas <adamjonas25@gmail.com>
2021-01-21 12:37:23 +0000

    Checkout :: pass spec for #total method

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -31,6 +31,18 @@ module SimpleCheckout
         checkout = Checkout.new(test_basket)
         expect(checkout.scan('milk')).to eq milk.price
       end
+
+      it 'increments the total price with the price of each scanned item' do
+        checkout = Checkout.new(test_basket)
+        checkout.scan('milk')
+        expect(checkout.total).to eq milk.price
+
+        checkout.scan('bread')
+        expect(checkout.total).to eq [milk, bread].map(&:price).sum
+
+        checkout.scan('steak')
+        expect(checkout.total).to eq [milk, bread, steak].map(&:price).sum
+      end
     end
 
     context '#remove_scanned_item' do
@@ -60,16 +72,10 @@ module SimpleCheckout
 
     context '#total' do
       let (:test_basket) { [milk, bread, steak] }
-      it 'increments the total price with the price of each scanned item' do
+      it 'returns the total price of all scanned items' do
         checkout = Checkout.new(test_basket)
-        checkout.scan('milk')
-        expect(checkout.total).to eq milk.price
-
-        checkout.scan('bread')
-        expect(checkout.total).to eq [milk, bread].map(&:price).sum
-
-        checkout.scan('steak')
-        expect(checkout.total).to eq [milk, bread, steak].map(&:price).sum
+        checkout.basket.map(&:name).each { |item| checkout.scan(item) }
+        expect(checkout.total).to eq checkout.basket.map(&:price).sum
       end
     end
   end
