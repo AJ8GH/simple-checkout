8ebd2601d85cf0331131eeba49bcd23e864cb5ed
Adam Jonas <adamjonas25@gmail.com>
2021-01-21 10:18:14 +0000

    Checkout :: pass spec for #basket getter method

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,7 +1,8 @@
 module SimpleCheckout
   class Checkout
+    attr_accessor :basket
     def initialize(basket)
-      
+      @basket = basket
     end
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,8 +1,23 @@
 module SimpleCheckout
   describe Checkout do
+    let(:example_basket) do
+      [{name: 'Milk', price: 50}, {name: 'Bread', price: 100}, {name: 'Steak', price: 18_000}]
+    end
+
     context '#initialize' do
       it 'takes an argument' do
         expect { Checkout.new(Object.new) }.to_not raise_error
+      end
+
+      it 'raises an execption if no argument is given' do
+        expect { Checkout.new }.to raise_exception
+      end
+    end
+
+    context '#basket' do
+      before(:each) { @checkout = Checkout.new(example_basket) }
+      it 'returns the items in the basket' do
+        expect(@checkout.basket).to eq example_basket
       end
     end
   end
